d66f191ddb1379cbb733e539e1efaa8568876798
fahriye zeynep çakır <96346390+zeynepfahriye@users.noreply.github.com>
2023-01-09 16:50:56 +0300

    checkbox control

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { toggle } from '../Redux/todos/todosSlice'
 function ToDoList() {
     const items = useSelector((state)=>state.todos.items)
-    console.log(items)
+    const dispatch = useDispatch()
   return (
     <ul className="todo-list">
         {items.map((item)=>(
             <li key={item.id} className={item.completed ?'completed':''}>
             <div className="view">
-                <input className="toggle" type="checkbox" />
+                <input className="toggle" type="checkbox" checked={item.completed} onChange={()=>dispatch(toggle({id:item.id}))}/>
                 <label>{item.title}</label>
                 <button className="destroy"></button>
             </div>
--- src/Redux/todos/todosSlice.js
+++ src/Redux/todos/todosSlice.js
@@ -18,8 +18,13 @@ initialState:{
 reducers:{
     addToDo:(state,action)=>{
         state.items.push(action.payload)
+    },
+    toggle:(state,action)=> {
+        const {id} = action.payload;
+        const item = state.items.find(item =>item.id ===id)
+        item.completed = !item.completed
     }
 }
 })
-export  const {addToDo} =todosSlice.actions
+export  const {addToDo,toggle} =todosSlice.actions
 export default todosSlice.reducer
